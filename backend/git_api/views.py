faced709fa25fdd60448b8b988f9870763120eed
Daniel M <belialdaniel@gmail.com>
2021-06-21 01:33:46 -0500

    close merge actions for pull request added (#9)

--- backend/git_api/views.py
+++ backend/git_api/views.py
@@ -5,6 +5,7 @@ from git_api import serializers
 from git_api.utils.mixins import GitData
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework import status
 
 Author = apps.get_model('git_api', 'Author')
 PullRequest = apps.get_model('git_api', 'PullRequest')
@@ -22,11 +23,27 @@ class PullRequestsViewSet(ModelViewSet):
 
     @action(detail=False, methods=['patch'])
     def merge(self, request,  pk=None):
-        breakpoint()
+        pull_request = self.get_object()
+        pull_request.status = PullRequest.Status.MERGED
+        try:
+            pull_request.save()
+        except:
+            return Response(dict(error="Could not merge pull request"),
+                                 status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response(serializer_class(pull_request).data, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['patch'])
     def close(self, request,  pk=None):
-        breakpoint()
+        pull_request = self.get_object()
+        pull_request.status = PullRequest.Status.CLOSE
+        try:
+            pull_request.save()
+        except:
+            return Response(dict(error="Could not close pull request"),
+                                 status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer_class(pull_request).data, status=status.HTTP_200_OK)
 
 
 class BranchesViewSe(ViewSet, GitData):
