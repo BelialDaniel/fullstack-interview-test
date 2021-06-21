4530a89b217e92fa272f0ca042543d80fc4802e3
Daniel Maldonado <belialdaniel@gmail.com>
2021-06-21 01:03:22 -0500

    Trying to fix CORS permission to create, clsoe and merge branch

--- backend/git_api/views.py
+++ backend/git_api/views.py
@@ -4,6 +4,7 @@ from django.apps import apps
 from git_api import serializers
 from git_api.utils.mixins import GitData
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 
 Author = apps.get_model('git_api', 'Author')
 PullRequest = apps.get_model('git_api', 'PullRequest')
@@ -17,6 +18,7 @@ class AuthorsViewSet(ModelViewSet):
 class PullRequestsViewSet(ModelViewSet):
     queryset = PullRequest.objects.all()
     serializer_class = serializers.PullRequestSerializer
+    permission_classes = [AllowAny, ]
 
     @action(detail=False, methods=['patch'])
     def merge(self, request,  pk=None):
