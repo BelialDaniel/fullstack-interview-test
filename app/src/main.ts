fb8f13b35022ab14a0fdc2754d9d398a7484aba9
Daniel Maldonado <belialdaniel@gmail.com>
2021-07-13 22:53:35 -0500

    Eslint config file added

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import MainLayout from "@/layouts/MainLayout.vue";
-import "@/assets/main.scss";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import MainLayout from "@/layouts/MainLayout.vue"
+import "@/assets/main.scss"
 
 createApp(App)
   .use(router)
   .use(store)
   .component("MainLayout", MainLayout)
-  .mount("#app");
+  .mount("#app")
